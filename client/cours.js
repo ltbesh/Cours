36d47a844bb8416972fb06ef5efb105ff54c334b
Lucas Tine-Beres <lucastineberes@gmail.com>
2013-10-01 15:28:02 +0800

    working on sliders

--- client/cours.js
+++ client/cours.js
@@ -27,11 +27,16 @@ Template.price_selector.rendered = function (){
 		values: [ 75, 300 ],
 	slide: function( event, ui ) {
 		$( "#amount" ).val( "€" + ui.values[ 0 ] + " - €" + ui.values[ 1 ] );
+	},
+	stop: function(event, ui){
+		Session.set("price_min", ui.values[ 0 ]);
+		Session.set("price_max", ui.values[ 1 ]);
 	}
 	});
-
 	$( "#amount" ).val( "$" + $( "#price_selector" ).slider( "values", 0 ) +
 	" - $" + $( "#price_selector" ).slider( "values", 1 ) );
+	Session.set("price_min", slider( "values", 0 );
+	Session.set("price_max", slider( "values", 1 );
 };
 
 
