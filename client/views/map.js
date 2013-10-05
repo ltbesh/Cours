649e58a4f2d3dcadf4e362309b7a3e98f275c73d
Lucas Tine-Beres <lucastineberes@gmail.com>
2013-10-05 21:32:25 +0800

    map displaying correctly an Firefox Chrome and IE

--- client/views/map.js
+++ client/views/map.js
@@ -1,7 +1,17 @@
 Template.map.rendered = function() {
- 	new GMaps({
- 	div: '#map',
- 	lat: -12.043333,
- 	lng: -77.028333
-	});
+    map = new GMaps({
+        el: '#map',
+        lat: 48.85432452980058,
+        lng: 2.3462677001953125,
+        zoomControl : true,
+        zoomControlOpt: {
+            style : 'MEDIUM',
+            position: 'TOP_LEFT'
+        },
+        zoom: 12,
+        panControl : false,
+        streetViewControl : true,
+        mapTypeControl: false,
+        overviewMapControl: false
+      });
 };
