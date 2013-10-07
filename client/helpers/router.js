47c20ceb07d1690b68c3dc98de67bfd2fedf6783
Lucas Tine-Beres <lucastineberes@gmail.com>
2013-10-07 20:46:35 +0800

    add google place autocompletion feature to the geographical search input

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,6 +1,6 @@
 Meteor.Router.add({
-	'/' : 'course_list_page',
-
+	'/' : 'home',
+	'/search' : 'course_list_page',
 	'/course/:_id' : {
 		to: 'course_detail_page', 
 		and: function(id) { Session.set('current_course', id); }
--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,11 +1,13 @@
 Template.home.rendered = function(){
-    var mapOptions = {
-      center: new google.maps.LatLng(-34.397, 150.644),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
-  
-  google.maps.event.addDomListener(window, 'load', initialize);
+    function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+      }
+    google.maps.event.addDomListener(window, 'onload', initialize);
+
 }
