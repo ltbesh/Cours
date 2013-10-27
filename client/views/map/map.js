a125705802340c5aa06ddda96a4ad1daf2a29728
Lucas Tine-Beres <lucastineberes@gmail.com>
2013-10-27 23:40:27 +0800

    fix marker remove and display should work fine

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -13,32 +13,37 @@ Template.map.rendered = function() {
             Session.get('geographical_search').location, 
             5);
 
-        if (places_cursor)
+        if (places_cursor){
             var places = places_cursor[0].fetch();
 
-        var places_id = _.pluck(places,'_id');
+            var places_id = _.pluck(places,'_id');
 
-        //Remove the markers that are no longer in the places_id array
-        _.each(gmaps.marker_data, function(marker){
-            if(! _.contains(places_id, marker.id)){
-                gmaps.remove_marker(marker.id);  
+            //Remove the markers that are no longer in the places_id array
+            for(var i = gmaps.marker_data.length - 1; i >= 0; i --){
+                var marker = gmaps.marker_data[i];
+                console.log()
+                if(! _.contains(places_id, marker.id)){
+                    gmaps.remove_marker_with_index(i); 
+                }
             }
-        });
-        _.each(places, function(place){
-            if(typeof(place.location.coordinates[1] !== undefined) && typeof(place.location.coordinates[0] !== undefined)){
-                var obj_marker = {
-                    id: place._id,
-                    lat: place.location.coordinates[1],
-                    lng: place.location.coordinates[0],
-                    title: place.title
-                };
-                var marker_not_exists = ! gmaps.marker_exists('id', obj_marker.id);
-                if(! gmaps.marker_exists('id', obj_marker.id))
-                    gmaps.add_marker(obj_marker);
-            }
-        });
 
-        gmaps.calc_bounds();
+            for(var i = 0, places_length = places.length; i < places_length; i ++){
+                var place = places[i];
+                if(typeof(place.location.coordinates[1] !== undefined) && typeof(place.location.coordinates[0] !== undefined)){
+                    var obj_marker = {
+                        id: place._id,
+                        lat: place.location.coordinates[1],
+                        lng: place.location.coordinates[0],
+                        title: place.title
+                    };
+                    var marker_not_exists = ! gmaps.marker_exists('id', obj_marker.id);
+                    if(! gmaps.marker_exists('id', obj_marker.id))
+                        gmaps.add_marker(obj_marker);
+                }
+            }
+
+            gmaps.calc_bounds();
+        }
     });
 };
 
