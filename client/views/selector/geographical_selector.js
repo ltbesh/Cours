bc20ab476dddaa9970047a0b4e54309216ec9219
Lucas Tine-Beres <lucastineberes@gmail.com>
2013-12-08 12:18:47 +0800

    geographical search working for city, postal code and adress

--- client/views/selector/geographical_selector.js
+++ client/views/selector/geographical_selector.js
@@ -10,43 +10,44 @@ Template.geographical_selector.rendered = function(){
         var g_place = autocomplete.getPlace();
         var locality = "";
         var postal_code = "";
-
         // Find the postal code and locality (city of the current search)
-        for (var i = g_place.address_components.length - 1; i >=0; i--){
-            var current_object = g_place.address_components[i];
-            if(_.contains(current_object.types,"postal_code")){
-                postal_code = current_object.long_name;
-            }
-            else if(_.contains(current_object.types,"locality")){
-                locality = current_object.long_name;
-            }
-        }
         if(g_place){
-            if(Session.get("search_page")){
-                Session.set("geographical_search", {
-                        address :g_place.formatted_address,
-                        location: {
-                            type : "Point", 
-                            coordinates: [g_place.geometry.location.lng(), 
-                                        g_place.geometry.location.lat()]
-                        },
-                        locality : locality,
-                        postal_code : postal_code,
-                        types : g_place.types
-                });
+            for (var i = g_place.address_components.length - 1; i >=0; i--){
+                var current_object = g_place.address_components[i];
+                if(_.contains(current_object.types,"postal_code")){
+                    postal_code = current_object.long_name;
+                }
+                else if(_.contains(current_object.types,"locality")){
+                    locality = current_object.long_name;
+                }
             }
-            else{
-                Session.set("edit_place_address", {
-                        address :g_place.formatted_address,
-                        location: {
-                            type : "Point", 
-                            coordinates: [g_place.geometry.location.lng(), 
-                                        g_place.geometry.location.lat()]
-                        },
-                        locality : locality,
-                        postal_code : postal_code,
-                        types : g_place.types
-                });                
+            if(g_place){
+                if(Session.get("search_page")){
+                    Session.set("geographical_search", {
+                            address :g_place.formatted_address,
+                            location: {
+                                type : "Point", 
+                                coordinates: [g_place.geometry.location.lng(), 
+                                            g_place.geometry.location.lat()]
+                            },
+                            locality : locality,
+                            postal_code : postal_code,
+                            types : g_place.types
+                    });
+                }
+                else{
+                    Session.set("edit_place_address", {
+                            address :g_place.formatted_address,
+                            location: {
+                                type : "Point", 
+                                coordinates: [g_place.geometry.location.lng(), 
+                                            g_place.geometry.location.lat()]
+                            },
+                            locality : locality,
+                            postal_code : postal_code,
+                            types : g_place.types
+                    });                
+                }
             }
         }
     });
